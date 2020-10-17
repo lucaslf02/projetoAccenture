b2eee138777d0f294ac37fd51e7a088a22d4187d
lucaslf02 <lucas_llf2010@hotmail.com>
2020-10-17 17:07:34 -0300

    implementação função decoder

--- controller/decodeController.js
+++ controller/decodeController.js
@@ -1,12 +1,15 @@
-
+import {decoder} from "../utils/decoder.js";
 
 const decode = async (req,res)=> {
-    const {code} = req.params;
-    console.log(req.params)
+    const {code} = req.params;        
     try{        
-        res.status(200).send("deu certo, code: "+code);
+        const text = await decoder(code);
+        if (text === ""){
+            throw new Error("Parametro invalido");
+        }
+        res.status(200).send("deu certo, code: "+ text);
     }catch(error){
-        res.status(500).send({message: "erro na requisição de decodificação; erro: "+ error});
+        res.status(500).send({message: "erro na requisição ao decodificar; erro: "+ error});
     }
 }
 
---
+++ utils/decoder.js
@@ -0,0 +1,43 @@
+//author: Gabriel
+var dict = {
+    "2": "A",
+    "22": "B",
+    "222": "C",
+    "3": "D",
+    "33": "E",
+    "333": "F",
+    "4": "G",
+    "44": "H",
+    "444": "I",
+    "5": "J",
+    "55": "K",
+    "555": "L",
+    "6": "M",
+    "66": "N",
+    "666": "O",
+    "7": "P",
+    "77": "Q",
+    "777": "R",
+    "7777": "S",
+    "8": "T",
+    "88": "U",
+    "888": "V",
+    "9": "W",
+    "99": "X",
+    "999": "Y",
+    "9999": "Z"
+  };
+  
+  async function decoder(strNumeros) {
+      try{
+        const arrayStrNumbers = strNumeros.split(" ");
+        const arrPalavra = await arrayStrNumbers.map(function(strNum) {
+          return dict[strNum];
+        });
+        return arrPalavra.join("");
+      }catch(error){
+        throw new Error(error);
+      }
+  }
+
+  export {decoder};
